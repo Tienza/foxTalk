5d84771ef8b48c697275a6705aaf9e0ecefc1bdc
Krisztian Koves <kkoves3@gmail.com>
2016-04-10 01:25:42 -0400

    Improved folder structure, removed unnecessary files

--- back-end/admin/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require('../includes/limbo_login_tools.php');
-    session_start();
-    
-    # If session is destroyed, redirect to main Limbo page
-    if(session_destroy())
-        load('login.php');
-?>
